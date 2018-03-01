fe7644a6ccc370830c7aa2ad5cea572025826114
NicoleHipolito <nhipolito1@gmail.com>
2018-02-28 19:51:00 -0800

    Set up README, finished user persistence, and set up initial timeline constraints

--- twitter_alamofire_demo/User.swift
+++ twitter_alamofire_demo/User.swift
@@ -9,11 +9,46 @@
 import Foundation
 
 class User {
-    
+    // MARK: Properties
+//    static var current: User?
     var name: String
+    var screenName: String?
+    
+    // For user persistance
+    var dictionary: [String: Any]?
+    private static var _current: User?
     
     init(dictionary: [String: Any]) {
+        self.dictionary = dictionary
         name = dictionary["name"] as! String
-
+        screenName = dictionary["screen_name"] as? String
     }
+    
+    static var current: User? {
+        get {
+            if _current == nil {
+                let defaults = UserDefaults.standard
+                if let userData = defaults.data(forKey: "currentUserData") {
+                    let dictionary = try! JSONSerialization.jsonObject(with: userData, options: []) as! [String: Any]
+                    _current = User(dictionary: dictionary)
+                }
+            }
+            return _current
+        }
+        set (user) {
+            _current = user
+            let defaults = UserDefaults.standard
+            if let user = user {
+                let data = try! JSONSerialization.data(withJSONObject: user.dictionary!, options: [])
+                defaults.set(data, forKey: "currentUserData")
+            } else {
+                defaults.removeObject(forKey: "currentUserData")
+            }
+        }
+    }
+//    init(dictionary: [String: Any]) {
+//        name = dictionary["name"] as! String
+//        screenName = dictionary["screen_name"] as? String
+//    }
+    // Initialize any other properties
 }
